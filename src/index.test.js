f96e344b9babb55aae25abb339af957766cd0dc2
Naoki Endoh <naoki.endoh@gmail.com>
2015-12-25 02:50:25 +0900

    Add unit test framework

---
+++ src/index.test.js
@@ -0,0 +1,8 @@
+var expect = require('chai').expect;
+var loveLiveCharacterNames = require('./index');
+
+describe('lovelive-character-names', function() {
+  it('should work!', function() {
+    expect(true).to.be.true;
+  });
+});
