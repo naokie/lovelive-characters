c98370cba2982003661b8d01b1aaaa04e7e6e1ef
Naoki Endoh <naoki.endoh@gmail.com>
2015-12-25 02:58:22 +0900

    Add unit test with mocha and chai

--- src/index.test.js
+++ src/index.test.js
@@ -2,7 +2,26 @@ var expect = require('chai').expect;
 var loveLiveCharacterNames = require('./index');
 
 describe('lovelive-character-names', function() {
-  it('should work!', function() {
-    expect(true).to.be.true;
+  describe('all', function() {
+    it('should be an array of strings', function() {
+      expect(loveLiveCharacterNames.all).to.satisfy(isArrayOfStrings);
+
+      function isArrayOfStrings(array) {
+        return array.every(function(item) {
+          return typeof item === 'string';
+        });
+      }
+    });
+
+    it('should contain `園田 海未`', function() {
+      expect(loveLiveCharacterNames.all).to.include('園田 海未');
+    });
+  });
+
+  describe('random', function() {
+    it('should return a random item from the loveLiveCharacterNames.all', function() {
+      var randamItem = loveLiveCharacterNames.random();
+      expect(loveLiveCharacterNames.all).to.include(randamItem);
+    });
   });
 });
